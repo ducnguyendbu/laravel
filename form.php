808db664362884549e1ac7b1a8f51b830f3ed2c5
ducnguyendbu <ducnguyendbu@gmail.com>
2021-03-16 20:31:44 +0700

    Cập nhật tính năng kiểm tra khoảng trắng

--- form.php
+++ form.php
@@ -1,29 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  // collect value of input field
+  $fname = trim($_POST['name']);
+  /*if (!preg_match("/^[a-zA-Z]*$/",$fname)) {
+  $nameErr = "<b style='color:red;'>Không được gõ ký tự trắng</b>";
+  echo $nameErr;
+  */
+  if (strpos($fname, " ")>0){
+  	 $nameErr = "<b style='color:red;'>Không được gõ ký tự trắng</b>";
+     echo $nameErr;
+  }
 
+}
+?>
 <html>
 <body>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" id="name" onkeyup="wakeUp()">
+  Name: <input type="text" name="name" id="name" onkeyup1="wakeUp()">
   <input type="submit">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $number = $_POST['name'];
-  if (is_numeric($number)) {
-    echo $number." la so";
-  } else {
-    echo $number." khong phai la so";
-  }
-}
-?>
+
 
 <script>
 function wakeUp() {
-  var str = document.getElementById("name").innerHTML;
-  alert(str);
+  var str = document.getElementById("name").value;
   var n = str.indexOf(" ");
+  if(n>=0){
+  	alert("Không được gõ ký tự trắng");
+  	document.getElementById("name").value = "";
+  }
   
 }
 </script>
